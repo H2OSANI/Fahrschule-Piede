6d4ca84a0fc745bf029dc98b90a47b74ca1697d0
H2OSANI <sandro.podo@gmx.de>
2023-11-03 20:18:49 +0100

    fix sitemap

--- fahschule-piede/next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-{import('next-sitemap').IConfig}
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://fahrschule-piede.de',
-    generateRobotsTxt: true,
-    outDir: './out' // (optional)
-    // ...other options
-  }
